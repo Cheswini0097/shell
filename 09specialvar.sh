408ff77cf51e9a505a83f49436a42c54a28ce174
chethan <cheth.cgamil.com>
2025-01-13 20:00:40 +0530

    specialvariable

--- 09specialvar.sh
+++ 09specialvar.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "All the variables passed through the script: $@"
 echo "Number of variable passed: $#"   # ro check the number of variable passed $#
+echo "Script Name: $0"
+echo "current working direcortory: $pwd"
+echo "Home directory of the current user: $Home"
+echo "pid of the script executeing now: $$"
+sleep 100 &
+echo "PID of the last background command: $!"
