cb708dc4ed7d50e82abd71b505971188cd36eb7f
chethan <cheth.cgamil.com>
2025-01-14 13:13:16 +0530

    Instalation

--- 11.installation .sh
+++ 11.installation .sh
@@ -12,12 +12,3 @@ dnf list installed git
 if [ $? -ne 0 ]
 then
     echo "git is not installed, git is installing"
-    dnf install git -y
-    if [ $? -ne -0 ]
-    then
-        echo "git installion is not sucess"
-        exit 1
-    else 
-        echo "git is already installed noting to do"
-
-    fi
