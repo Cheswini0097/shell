91ecb0d3970723a93bf258d8db07178fbb3675b0
chethan <cheth.cgamil.com>
2025-01-14 12:48:13 +0530

    Instalation

--- 11.installation .sh
+++ 11.installation .sh
@@ -8,3 +8,18 @@ then
     echo "Run the script with the Root privilages"
     exit 1
 fi
+dnf list installed git # to check wheather git is intalled or not
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, going to intall the git"
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "git installation is not sucess... check it"
+        exit1
+    else
+        echo "Git installation is sucess.... nothing to do"
+    fi
+else
+    echo "git is allready installed"
+            
