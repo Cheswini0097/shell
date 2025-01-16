cd18d26aebc90a46008f1b08c4ab3f7a62948801
chethan <cheth.cgamil.com>
2025-01-16 20:26:30 +0530

    disk_usgae

--- vowels.sh
+++ vowels.sh
@@ -2,6 +2,6 @@
 read -p "enter the string: " string
 string=$(echo $string  | tr '[:upper:]' '[:lower:]')
 vowels_count=$(echo $string | grep "[aeiou]" | wc -l)
-vovels=$(echo $string | grep -o -i "[aeiou]")
+vovels=$(echo $string | grep "[aeiou]")
 echo "number of vowels in the string:", $vowels_count
 echo "Vovels in the string:", $vovels  
