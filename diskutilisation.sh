3461bf9584bf4de8c2e6e79b02f0fa6f304bd669
chethan <cheth.cgamil.com>
2025-01-16 15:14:56 +0530

    disk_usgae

--- diskutilisation.sh
+++ diskutilisation.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=30 #real projects, it is usually 75
+# DISK_USAGE=$(df -hT | grep xfs)
+# DISK_THRESHOLD=30 #real projects, it is usually 75
 
 
-while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-do
-    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
+# while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+# do
+#     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+#     PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+#     if [ $USAGE -ge $DISK_THRESHOLD ]
+#     then
+#         echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+#     fi
+# done <<< $DISK_USAGE
+
+
+DISK_USAGE=$(df -hT)
+DISK_THRESHOLD=5%
+
+while IFS= read -r line
+do 
+    USAGE=$(echo $line)
+    PARTITION=$(echo $line)
+    if [if $USAGE is -ge $DISK_THRESHOLD]
     then
-        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+        echo "$PARTITION is more than $DISK_THRESHOLD, current value is: $USAGE, please check the partion"
     fi
 done <<< $DISK_USAGE
