8bc2a217ca043fc784c5dc38824fcfe4a06f3bcf
chethan <cheth.cgamil.com>
2025-01-16 19:47:00 +0530

    disk_usgae

--- vowels.sh
+++ vowels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter the String" string
 string=$(echo $string | tr '[:upper:]' '[:lower:]' )
-vovels=$(echo $string | grep  "[aeiou]" | wc -l)
-echo "Number of vovels in the string: $vovels"
+count=$(echo $string | grep  "[aeiou]" | wc -l)
+echo "Number of vovels in the string: $count"
 
 
