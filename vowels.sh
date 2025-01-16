0c4b73f1821cec6787765adef60e3727f491f034
chethan <cheth.cgamil.com>
2025-01-16 20:01:36 +0530

    disk_usgae

--- vowels.sh
+++ vowels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter the String" string
 string=$(echo $string | tr '[:upper:]' '[:lower:]' )
-vowels=$(echo $string | grep  "[aeiou]")
+vowels=$(echo $string | grep  "[aeiou]" $string)
 echo "vovels in the string are: " , $vowels
 
 |
