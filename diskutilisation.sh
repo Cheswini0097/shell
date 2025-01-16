1638e3f0b9381107c4cea66b287e328d538e7a54
chethan <cheth.cgamil.com>
2025-01-16 16:23:11 +0530

    disk_usgae

--- diskutilisation.sh
+++ diskutilisation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-89DISK_USAGE=$(df -hT)
+DISK_USAGE=$(df -hT)
 DISK_THRESHOLD=5
 
 while IFS= read -r line
 do 
-    USAGE=$(echo $line | awk '{print $6}' |  sed 's/%//)
+    USAGE=$(echo $line | awk '{print $6}')
     PARTITION=$(echo $line | awk '{print $7}')
     if [ "$USAGE" -ge "$DISK_THRESHOLD" ]
     then
