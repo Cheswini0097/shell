8e3eb5c470928012276836dab6e6814e225f5e0d
chethan <cheth.cgamil.com>
2025-01-16 14:26:11 +0530

    disk_usgae

--- diskutilisation.sh
+++ diskutilisation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=5
 
-while IFS= read -r line
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5 #real projects, it is usually 75
+
+
+while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
